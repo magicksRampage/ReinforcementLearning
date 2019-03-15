f7f894d6af37e44ac99b8f64c2b86cfa60ced5b2
Rumgar <tim.unverzagt@googlemail.com>
2019-03-15 17:30:09 +0100

    Add: Step 3.1/6 minimize half of the dual (alpha)

--- REPS/reps.py
+++ REPS/reps.py
@@ -93,12 +93,30 @@ def evaluate_state_action_kernel(samples):
     return state_action_kernel
 
 
+def evaluate_state_transition_kernel(samples):
+    """
+
+    :param samples: set of state-action pairs available to calculate the state-transition kernel
+    :return: Matrix (len(samples))X(len(samples)) defining the state-transition kernel
+    """
+    number_of_samples = np.shape(samples)[0]
+    state_transition_kernel = np.zeros((number_of_samples, number_of_samples))
+    sample_i = None
+    sample_j = None
+    for i in range(0, number_of_samples):
+        sample_i = samples[i]
+        for j in range(0, number_of_samples):
+            sample_j = samples[j]
+            state_transition_kernel[i][j] = gaussian_kernel(sample_i[0], sample_j[2])
+    return state_transition_kernel
+
+
 def gaussian_kernel(vec1, vec2, bandwidth_matrix=None):
     """
 
-    :param vec1: first argument in vector form (scalars might cause problems due to np.transpose())
-    :param vec2: second argument in vector form (scalars might cause problems due to np.transpose())
-    :param bandwidth_matrix: Matrix defining the free bandwith parameters of a gaussian kernel (must be len(vec1) == len(vec2)
+    :param vec1: first argument in array form (scalars will cause problems due to np.transpose())
+    :param vec2: second argument in array form (scalars will cause problems due to np.transpose())
+    :param bandwidth_matrix: Matrix defining the free bandwidth parameters of a gaussian kernel (must be len(vec1) == len(vec2)
     :return: scalar result of the kernel evaluation
     """
     dif_vec = vec1-vec2
@@ -107,37 +125,136 @@ def gaussian_kernel(vec1, vec2, bandwidth_matrix=None):
     return np.exp(np.matmul(np.matmul(-np.transpose(dif_vec), bandwidth_matrix), dif_vec))
 
 
-def calculate_beta(state, action, samples, K_sa, l_c=1.0):
+def calculate_beta(state, action, samples, transition_kernel, l_c=1.0):
     """
 
-    :param state: state-argument put into beta(s,a)
-    :param action: action-argument put into beta(s,a)
+    :param state: state-argument for which to evaluate beta(s,a)
+    :param action: action-argument for which to evaluate beta(s,a)
     :param samples: set of state-action pairs available to calculate beta(s,a)
-    :param K_sa: the pre-calculated state-action kernel matrix
+    :param transition_kernel: the pre-calculated state-action kernel matrix
     :param l_c: regularization coefficient
-    :return:
+    :return: beta(s,a)
     """
     number_of_samples = np.shape(samples)[0]
-    k_sa = np.zeros((number_of_samples, 1))
+    state_action_vec = np.zeros((number_of_samples, 1))
     sample = None
     for i in range(0,number_of_samples):
         sample = samples[i]
         state_kval = gaussian_kernel(sample[0], state)
         action_kval = gaussian_kernel(np.array([sample[1]]), np.array([action]))
-        k_sa[i] = state_kval * action_kval
+        state_action_vec[i] = state_kval * action_kval
     reg_mat = np.multiply(l_c, np.identity(number_of_samples))
-    beta = np.matmul(np.transpose(np.add(K_sa, reg_mat)),k_sa)
+    beta = np.matmul(np.transpose(np.add(transition_kernel, reg_mat)), state_action_vec)
     return beta
 
 
-def fast_minimization():
+def calculate_embedding_vector(state, action, samples, transition_kernel):
+    """
+
+    :param state: state-argument for which to evaluate the embedding vector
+    :param action: action-argument for which to evaluate the embedding vector
+    :param samples: set of state-action pairs available to calculate the embedding vector
+    :param transition_kernel: the pre-calculated state-action kernel matrix
+    :param l_c: regularization coefficient
+    :return: the embedding vector (s,a) needed for the bellman-error
+    """
+
+    beta = calculate_beta(state, action, samples, transition_kernel)
+
+    number_of_samples = np.shape(samples)[0]
+    transition_vec = np.zeros((number_of_samples, 1))
+    sample = None
+    for i in range(0, number_of_samples):
+        sample = samples[i]
+        transition_vec[i] = gaussian_kernel(sample[0], state)
+
+    embedding_vec = np.add(np.matmul(transition_kernel, beta), -transition_vec)
+    return embedding_vec
+
+
+def calculate_bellman_error(reward, alpha, embedding_vector):
+    """
+
+    :param reward:
+    :param alpha:
+    :param embedding_vector:
+    :return:
+    """
+    # TODO: Comment
+    error = reward + np.matmul(np.transpose(alpha), embedding_vector)
+    return error
+
+
+def minimize_dual_for_alpha(initial_alpha, eta, samples, number_of_iterations=10):
+    """
+
+    :param initial_alpha:
+    :param eta:
+    :param samples:
+    :param number_of_iterations:
+    :return:
+    """
+    # TODO: Comment
+    number_of_samples = np.shape(samples)[0]
+    transition_kernel = evaluate_state_transition_kernel(samples)
+    alpha = initial_alpha
+
+    embedding_vectors = np.zeros((number_of_samples, number_of_samples))
+    for i in range(0, number_of_samples):
+        sample = samples[i]
+        embedding_vectors[:, i] = calculate_embedding_vector(sample[0], sample[1], samples, transition_kernel).reshape(number_of_samples,)
+
+    # Initialize values but once
+    bellman_errors = np.zeros((number_of_samples, 1))
+    temp = np.zeros((number_of_samples, 1))
+    weights = np.zeros((number_of_samples, 1))
+    denominator = None
+    partial = np.zeros((number_of_samples, 1))
+
+    for descent in range(0, number_of_iterations):
+
+        sample = None
+        for i in range(0, number_of_samples):
+            sample = samples[i]
+            bellman_errors[i] = calculate_bellman_error(sample[3], alpha, embedding_vectors[:, i])
+
+        for i in range(0, number_of_samples):
+            denominator = 0.0
+            for j in range(i, number_of_samples):
+                denominator += np.exp(np.divide(bellman_errors[j], eta))
+            weights[i] = np.divide(np.exp(np.divide(bellman_errors[i], eta)), denominator)
+
+        partial = np.zeros((number_of_samples, 1))
+        for i in range(0, number_of_samples):
+            partial = np.add(partial, np.multiply(weights[i], embedding_vectors[:, i]).reshape((number_of_samples, 1)))
+
+        alpha = np.add(alpha, -partial)
+
+    return alpha
+
+
+def minimize_dual_for_eta():
+    # Constrained opt
     # TODO: implement
     return 0.0
 
 
-def constrained_minimization():
-    # TODO: implement
-    return 0.0
+def strictly_bigger(vec1, vec2):
+    """
+
+    :param vec1:
+    :param vec2:
+    :return:
+    """
+    # TODO: Comment
+    booleans = np.greater(vec1 , vec2)
+    bigger = True
+    for i in range(0, np.shape(booleans)[0]):
+        if bigger:
+            bigger = booleans[i]
+        else:
+            break
+    return bigger
 
 
 def update_step(old_policy):
@@ -145,28 +262,28 @@ def update_step(old_policy):
     """ 1. generate roll-outs according to pi_(i-1) """
 
     samples = generate_episode(ENV_PENDULUM, POLICY_GAUSSIAN)
+    number_of_samples = np.shape(samples)[0]
 
     """ 2. calculate kernel embedding strengths """
     """ Which means prepare the Kernelmatrix so you can evaluate beta(s,a) """
     K_sa = evaluate_state_action_kernel(samples)
-    test = calculate_beta(samples[0][0], samples[0][1], samples, K_sa)
 
     """ 3. minimize kernel-based dual """
 
     # iterate coordinate-descent (till constraints are sufficiently fulfilled)
-    alpha = np.inf
-    eta = np.inf
+    alpha = np.ones((number_of_samples, 1))*0.01
+    eta = 0.01
     # convergence threshold
     epsilon = 0.1
-    while (alpha > epsilon) | (epsilon > epsilon):
+    for i in range(0, 10):
+    #while (strictly_bigger(alpha, np.ones((number_of_samples, 1))*epsilon)) | (eta > epsilon):
 
         # fast unconstrained convex optimization on alpha (fixed iterations)
-        for iteration in range(0,10):
-            alpha = fast_minimization()
+        alpha = minimize_dual_for_alpha(alpha, eta, samples)
 
         # constrained minimization on eta (fixed iterations)
-        for iteration in range(0,10):
-            eta = constrained_minimization()
+        for iteration in range(0, 10):
+            eta = minimize_dual_for_eta()
 
     """ 4. calculate kernel-based Bellman errors """
 
