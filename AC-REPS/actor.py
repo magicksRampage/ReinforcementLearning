Bugfix: kl-distance was sneakly negative | Add: Gaussian noice in policy

AC-REPS/actor.py
@@ -9,7 +9,7 @@ import numpy as np
 import model
 import time
 from scipy.optimize import minimize
-from scipy.stats import multivariate_normal as mvn
+from scipy.stats import norm
 
 
 class Actor:
@@ -27,10 +27,11 @@ class Actor:
         self.v_critic = v_critic
         # The old actor will be none in the first run so we explore randomly
         self.old_actor = old_actor
-        self.weights = None
+        self.regulated_weights = None
         self._calculate_weights()
-        self.model = model.Model(model.RANDOM_RBFS,
+        self.model = model.Model(model.POLYNOMIAL_LINEAR,
                                  np.shape(self.samples[0][0])[0])
+        self.variance = 1.0
         self._fit()
 
     def _calculate_weights(self):
@@ -40,44 +41,58 @@ class Actor:
         vc = self.v_critic
         qc = self.q_critic
         weights = np.zeros((number_of_samples, 1))
+        weight_regulator = -np.inf
         # Numeric Regulation
         for i in range(0, number_of_samples):
+            exponent = (qc.estimate_q(states[i], actions[i]) - vc.estimate_v(states[i])) / vc.eta
+            if exponent > weight_regulator:
+                weight_regulator = exponent
+        for i in range(0, number_of_samples):
             # TODO: Ensure stability
-            # !!! Most sensible point for numerics !!!
-            weights[i] = np.exp((qc.estimate_q(states[i], actions[i])
-                                 - vc.estimate_v(states[i]))
-                                / vc.eta)
-        self.weights = weights
+            exponent = (qc.estimate_q(states[i], actions[i]) - vc.estimate_v(states[i])) / vc.eta
+            weights[i] = np.exp(exponent - weight_regulator)
+        self.regulated_weights = weights
 
     def _fit(self):
         print("Handing of work to scipy")
         prev_time = time.clock()
-        initial_values = self.model.parameters
+        initial_values = np.append(self.model.parameters, [self.variance])
         constraints = ()
         for i in range(0, initial_values.size):
             constraints += ((0, None),)
-        res = minimize(self._calc_kl_distance,
+        res = minimize(self._wrap_inputs,
                        initial_values,
                        jac=False,
                        method='SLSQP',
                        bounds=constraints,
                        options={'ftol': 1e-6, 'disp': True})
-        self.model.parameters = res.x
+        self.model.parameters = res.x[0:-1]
         print(res)
         print("Fitting Actor_Time: ", time.clock() - prev_time)
 
-    def _calc_kl_distance(self, parameters):
-        self.model.parameters = parameters
+    def _wrap_inputs(self, values):
+        self.model.parameters = values[0:-1]
+        self.variance = values[-1]
+        return self._calc_regulated_kl_distance()
+
+    def _calc_regulated_kl_distance(self):
         states = self.samples[0]
+        actions = self.samples[1]
         number_of_samples = np.shape(states)[0]
-        kl_distance = 0.0
+        regulated_sum = 0.0
         for i in range(0, number_of_samples):
-            if self.model.evaluate(states[i]) <= 0.0:
-                print("model unfit for optimizing over kl-distance")
-            kl_distance += (1 / number_of_samples) * self.weights[i]\
-                            * np.log(self.model.evaluate(states[i]))
-        return kl_distance
+            pi = self._policy_probability(states[i], actions[i])
+            if pi <= 0.0:
+                return np.inf
+            regulated_sum += self.regulated_weights[i] * np.log()
+        # Ignore the regulator term as it is constant
+        regulated_kl_distance = -regulated_sum * (1 / number_of_samples)  # +np.exp(weight_regulator)
+        return regulated_kl_distance
+
+    def _policy_probability(self, state, action):
+        mean = self.model.evaluate(state)
+        return norm(loc=mean, scale=self.variance).pdf(action)
 
     def act(self, state):
-        act = self.model.evaluate(state)
-        return act
+        mean = self.model.evaluate(state)
+        return np.clip(norm.rvs(loc=mean, scale=self.variance))

AC-REPS/agent.py
@@ -13,12 +13,10 @@ EPISODE_LENGTH = 100
 
 class Agent:
 
-    def __init__(self, env):
-        self.environment_name = env
-        env = gym.make(self.environment_name)
-        self.state_dimensions = np.reshape(np.append(env.observation_space.low, env.observation_space.high), (2, -1)).transpose()
-        self.action_dimension = [env.action_space.low[0], env.action_space.high[0]]
-        env.close()
+    def __init__(self, environment_name):
+        self.env = gym.make(environment_name)
+        self.state_dimensions = np.reshape(np.append(self.env.observation_space.low, self.env.observation_space.high), (2, -1)).transpose()
+        self.action_dimension = [self.env.action_space.low[0], self.env.action_space.high[0]]
         # Will be containing the values of the most recent train_step()
         self.samples = None
         self.q_critic = None
@@ -49,8 +47,7 @@ class Agent:
         self.actor = ac.Actor(self.samples, self.q_critic, self.v_critic, self.actor)
 
     def generate_episode(self):
-        env = gym.make(self.environment_name)
-        norm_prev_obs = env.reset()
+        norm_prev_obs = self.env.reset()
         # Normalize the observations
         for i in range(0, np.shape(self.state_dimensions)[0]):
             norm_prev_obs[i] = self._normalize(norm_prev_obs[i], self.state_dimensions[i][0], self.state_dimensions[i][1])
@@ -64,13 +61,15 @@ class Agent:
         else:
             samples = self.samples
 
-        for i in range(0, EPISODE_LENGTH):
+        done = False
+        while not done:
             if self.actor is None:
                 # If you haven't trained an actor explore randomly
-                action = self._denormalize(np.random.random(), self.action_dimension[0], self.action_dimension[1])
+                action = self._denormalize(np.clip(np.random.normal(0.5, 0.5), 0, 1), self.action_dimension[0], self.action_dimension[1])
             else:
                 action = self._denormalize(self.actor.act(norm_prev_obs), self.action_dimension[0], self.action_dimension[1])
-            norm_obs, reward, done, info = env.step(np.array([action]))
+            norm_obs, reward, done, info = self.env.step(np.array([action]))
+            # print(done)
             # Normalize the observations
             for i in range(0, np.shape(self.state_dimensions)[0]):
                 norm_obs[i] = self._normalize(norm_obs[i], self.state_dimensions[i][0], self.state_dimensions[i][1])
@@ -79,8 +78,8 @@ class Agent:
             norm_next_states += (norm_obs,)
             rewards += (reward,)
             norm_prev_obs = norm_obs
-            env.render()
-        env.close()
+            self.env.render()
+        self.env.close()
 
         number_of_samples = np.shape(norm_states)[0]
         # Reshape arrays to catch shape of (n,) and replace it with (n,1)
@@ -91,9 +90,11 @@ class Agent:
         self.samples = samples
 
     def _normalize(self, value, min_value, max_value):
+        # Scale value to the interval [0, 1]
         return (value - min_value) / (max_value - min_value)
 
     def _denormalize(self, normalized_value, min_value, max_value):
+        # Scale value back from interval [0, 1]
         return (normalized_value * (max_value - min_value)) + min_value
 
 

AC-REPS/model.py
@@ -39,7 +39,6 @@ class Model:
             self.number_of_basis_functions = self.number_of_parameters
 
         elif self.model_name == RANDOM_RBFS:
-            print("RBFS are many times (~ 10-100x) slower than polynomial basis-functions!")
             if self.number_of_basis_functions is None:
                 print("No number of basis_functions has been chosen for RBFS.")
                 print("The model will try to evenly tile the input-space.")
@@ -48,8 +47,8 @@ class Model:
                 self.number_of_parameters = np.power(2, self.len_in) + 1
             else:
                 self.number_of_parameters = self.number_of_basis_functions
-            # Inner parameters contain multivariate mean
-            self.number_of_inner_parameters = len_in * self.number_of_parameters
+            # Inner parameters contain multivariate means and one general multivariate variance
+            self.number_of_inner_parameters = len_in * self.number_of_parameters + len_in
 
         else:
             print("Model name is not one of the designated model names!")
@@ -60,7 +59,7 @@ class Model:
     def _initialize_parameters(self):
         # Let every basis function influence the outcome at first
         if self.model_name == RANDOM_RBFS:
-            self.inner_parameters = np.zeros((self.number_of_inner_parameters, 1))
+            self.inner_parameters = np.ones((self.number_of_inner_parameters, 1))
             if self.number_of_basis_functions is None:
                 # Place Gaussians on the corners of the space
                 for i in itertools.product([0, 1], repeat=self.len_in):
@@ -69,7 +68,7 @@ class Model:
                         means_processed += i[j] * np.power(2, self.len_in - (j+1))
                     for j in range(0, self.len_in):
                         self.inner_parameters[int(means_processed * self.len_in + j)] = i[j]
-                # Place one wider gaussian over the center of the space
+                # Place one gaussian over the center of the space
                 for i in range(0, self.len_in):
                     self.inner_parameters[-(i+1)] = 0.5
             else:
@@ -107,16 +106,20 @@ class Model:
                 n = self.number_of_basis_functions
             # Interpret the space under one sigma as the relevant support for a gaussian
             # Now adjust the support that each gaussian "covers one corner of your space"
-            var = np.sqrt(dim) * (1 / 2)
+            in_var = self.inner_parameters[-(dim+1):-1]
             for i in range(0, n):
-                myu = self.inner_parameters[i*dim:(i+1)*dim]
-                # This call takes up >90% of the eval_function-time and makes RBFS really slow
-                evaluated_basis_functions[i] = stats.multivariate_normal(mean=np.reshape(myu, (-1,)), cov=var).pdf(args)
+                mean = self.inner_parameters[i*dim:(i+1)*dim]
+                # Using the pseudo-multi-variate gaussian improves speed drastically
+                evaluated_basis_functions[i] = self._evaluate_pseudo_mv_gaussian(args, mean, in_var)
 
-        if self.model_name == RANDOM_RBFS:
-            result = np.dot(self.parameters[:self.number_of_basis_functions], np.array(evaluated_basis_functions))
-        else:
-            result = np.dot(self.parameters, np.array(evaluated_basis_functions))
+        result = np.dot(self.parameters, np.array(evaluated_basis_functions))
         return result
 
+    def _evaluate_pseudo_mv_gaussian(self, args, mean, inverted_var):
+        # Ignore any constant parts of a multivariate gaussian as the parameters adopt anyway
+        diff_vec = np.reshape(args, (-1,1)) - np.reshape(mean, (-1,1))
+         # Handle the variances as if the covariance-matrix had already been inverted. Optimization will adapt
+        inv_covar = np.diag(np.reshape(inverted_var,(-1,)))
+        result = np.matmul(np.matmul(np.transpose(diff_vec), inv_covar), diff_vec) [0]
+        return result
 

AC-REPS/q_critic.py
@@ -5,8 +5,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-# TODO: Handle Hyperparameters correctly
+# TODO: Hyper-parameter: Decay-rate for the rewards and Qs
 DECAY = 0.98
+# TODO: Computability: max_epoches
+MAX_EPOCHS = 1000
 
 
 class QCritic:
@@ -30,7 +32,7 @@ class QCritic:
                                    nn.Linear(self.n_h, self.n_out))
         self.criterion = nn.MSELoss()
         # TODO: Hyper-parameter: learning_rate
-        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4)
         self._fit()
 
     def _fit(self):
@@ -47,16 +49,11 @@ class QCritic:
         # Switch to float because the NN demands it ?.?
         model_input = model_input.float()
         rewards = rewards.float()
-        # TODO: Is the target correct here?
-        # TODO: Specify Hyperparameter (learning-rate, epoches)
-        # number_of_epochs = 500
-        # for epoch in range(0, number_of_epochs):
         loss = np.inf
         epoch = 0
         # TODO: Hyper-parameter: loss_threshold
-        loss_threshold = 1e-10
-        # TODO: Safety: max_epoches
-        while (loss > loss_threshold) & (epoch < 5000):
+        loss_threshold = 1e-6
+        while (loss > loss_threshold) & (epoch < MAX_EPOCHS):
             # Forward Propagation
             predictions = self.model(model_input)
             number_of_predictions = predictions.size()[0]
@@ -72,10 +69,8 @@ class QCritic:
             for td_step in range(1, 1+td_range):
                 if td_step == td_range:
                     target[0:target.size()[0] - td_step] += np.power(DECAY, td_step) * predictions[td_step:]
-                    # target[target.size()[0] - td_step:] += np.power(DECAY, i) * average_prediction * torch.ones((td_step, 1))
                 else:
                     target[0:target.size()[0] - td_step] += np.power(DECAY, td_step) * rewards[td_step:]
-                    # target[target.size()[0] - td_step:] += np.power(DECAY, i) * average_reward * torch.ones((td_step, 1))
             # Compute and print loss
             loss = self.criterion(predictions, target)
             epoch += 1
@@ -85,15 +80,14 @@ class QCritic:
             self.optimizer.zero_grad()
 
             # perform a backward pass (backpropagation)
-            # if epoch == (number_of_epochs - 1):
-            if (loss < loss_threshold) | (epoch == 2000):
+            if (loss < loss_threshold) | (epoch == MAX_EPOCHS):
                 loss.backward()
             else:
                 loss.backward(retain_graph=True)
 
             # Update the parameters
             self.optimizer.step()
-        print([predictions, rewards])
+        # print([predictions, rewards])
         # print(number_of_predictions)
 
     def estimate_q(self, state, action):