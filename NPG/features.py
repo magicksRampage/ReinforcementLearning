ced90cb19c21e51aadbc3d15aa156d32c4afe1d0
Marc-André Reichelt <marc.reichelt94@gmail.com>
2019-04-12 00:49:32 +0200

    added comments

--- NPG/features.py
+++ NPG/features.py
@@ -5,6 +5,15 @@ import matplotlib.pyplot as plt
 import itertools
 
 def fourier(o, P, v, phi):
+    """
+    calculates fourier features
+
+    :param o: vector of observations
+    :param P: weight matrix for linear combination of observations
+    :param v: wavelength parameter (float)
+    :param phi: vector of offsets, one for each feature
+    :return: vector of fourier features
+    """
     y = []
     numfeat = len(phi)
     for i in range(numfeat):
@@ -17,6 +26,13 @@ def fourier(o, P, v, phi):
     return np.array(y)
 
 def radial_basis_functions(o, p):
+    """
+    calculates rbf features
+
+    :param o: vector of observations
+    :param p: feature parameters [P, 0, sigma]
+    :return: vector of rbf features
+    """
     rbf = []
     P = np.array(p[0])
     sigma = np.array(p[2])
@@ -33,6 +49,13 @@ def radial_basis_functions(o, p):
     return np.array(rbf)
 
 def polynomial(o, p):
+    """
+    calculates polynomial features
+
+    :param o: vector of observations
+    :param p: feature parameters [P, 0, 0]
+    :return: vector of polynomial features
+    """
     poly = []
     P = p[0]
     numfeat = P.shape[0]
@@ -46,6 +69,13 @@ def polynomial(o, p):
     return np.array(poly)
 
 def tiling(o, p):
+    """
+    calculates tile features constrained in 2 dimensions
+
+    :param o: vector of observations
+    :param p: feature parameters [P, 0, 0]
+    :return: vector of tile features
+    """
     tiles = []
     P = p[0]
     numfeat = P.shape[0]
@@ -60,16 +90,28 @@ def tiling(o, p):
     return np.array(tiles)
 
 def getphi(I,env):
+    """
+    get random offsets for fourier features in (-pi, pi)
+
+    :param I: number of features
+    :param env: gym learning environment - deprecated
+    :return: vector of random offsets
+    """
     phi = []
-    #limits = env.observation_space.high
-    #return np.array(limits) * 2
-    #return np.ones([I])
     for i in range(I):
         phi.append(random.random() * 2 * math.pi - math.pi)
     return phi
 
 
 def getP(I,J,env):
+    """
+    get random weight matrix for feature construction
+
+    :param I: number of features
+    :param J: number of observations
+    :param env: gym learning environment
+    :return: random weight matrix (:param I:,:param J:)
+    """
     P = []
     limits = env.observation_space.high
     for i in range(I):
@@ -79,11 +121,18 @@ def getP(I,J,env):
                 Pi.append(2 * random.random() - 1)
             else:
                 Pi.append(0)
-            #Pi.append(random.gauss(0,1))
         P.append(Pi)
     return P
 
 def getP_polynomial(numobs, degree):
+    """
+    get exponent matrix for polynomial features
+    assumes weak coupling. only 2nd degree terms have more than exponent > 0
+
+    :param numobs: number of observations
+    :param degree: maximum degree of the polynomial
+    :return: exponent matrix (number of features, :param numobs:)
+    """
     numfeat = 0
     for i in range(degree):
         if i+1 == 2:
@@ -107,6 +156,15 @@ def getP_polynomial(numobs, degree):
     return P
 
 def getP_2dtiles(numfeat, numobs):
+    """
+    get constraint matrix for tile features
+    assumes weak coupling. only 2 observations are constrained for each tile
+
+    :param numfeat: number of features
+    :param numobs: number of observations
+    :return: constraint matrix (number of features, :param numobs:)
+        each row is a constraint of form [index(A), lower bound A, upper bound A, index(B), lower bound B, upper bound B]
+    """
     P = np.zeros([numfeat, 6])
     for feat in range(numfeat):
         for i in range(2):
@@ -125,6 +183,13 @@ def getP_2dtiles(numfeat, numobs):
     return P
         
 def getP_rbf(numobs, grain = 3):
+    """
+    get matrix of equidistant means for radial basis functions
+
+    :param numobs: number of observations
+    :param grain: how many basis functions to generate for every dimension
+    :return: matrix (number of features, :param numobs:)
+    """
     elements = []
     for i in range(grain):
         elements.append(-1 + i* (2.0 / (grain - 1)))
@@ -134,6 +199,24 @@ def getP_rbf(numobs, grain = 3):
     return np.array(P)
 
 def initialize_feature_parameters(num_features = 0, num_observations = 0, env = None, feature_type = "linear", sigma = 1, random = False):
+    """
+    construct feature parameters
+
+    :param num_features: number of features, except for polynomials where it is the degree of the polynomial
+    :param num_observations: number of observations
+    :param env: gym learning environment
+    :param feature_type: string specifying the kind of features to calculate - options are:
+        "linear" - use observations as features
+        "fourier" - fourier basis functions
+        "rbf" - radial basis functions
+        "polynomial" - polynomial basis functions
+        "2dtiles" - (overlapping) tiles constrained in 2 dimensions
+    :param sigma: standard deviation for rbf features
+    :param random: boolean that determines if rbfs should be random or equidistant
+    :return: feature parameters [(N,M), float, (N) or (M) or (0)] or None
+        where N = :param num_features:
+        and M = :param num_observations:
+    """
     if feature_type == "linear":
         return
     elif feature_type == "polynomial":
@@ -152,6 +235,16 @@ def initialize_feature_parameters(num_features = 0, num_observations = 0, env = 
         return [P, v, phi]
 
 def gradient_update(stepsize = 0.1, value = [], feature_params = [], x = 0, y = 0):
+    """
+    semi-gradient update for a linear value function
+
+    :param stepsize: how far to move in the direction of the new parameter estimate (float)
+    :param value: vector of value function parameters
+    :param feature_params: feature parameters [P, 0, sigma]
+    :param x: point at which to evaluate the value function (float)
+    :param y: value objective to move towards (float)
+    :return: vector of updated value parameters
+    """
     features = radial_basis_functions(o = [x], p = feature_params)
     y_est = np.dot(value, features)
     value += stepsize * features * (y - y_est)
@@ -159,6 +252,13 @@ def gradient_update(stepsize = 0.1, value = [], feature_params = [], x = 0, y = 
     return value
 
 def plot(value = [], feature_params = [], validation_set = []):
+    """
+    plot the true function (a sinus) and its estimate
+
+    :param value: vector of value function parameters
+    :param feature_params: feature parameters [P, 0, sigma]
+    :param validation_set: vector of points at which to evaluate the value function
+    """
     y_est = []
     y_true = []
     for x in validation_set:
@@ -171,6 +271,10 @@ def plot(value = [], feature_params = [], validation_set = []):
     
 
 def test_features():
+    """
+    learn to approximate a sinus function using rbf features and semi-gradient updates
+    periodically plot the progress
+    """
     P = []
     num_feat = 50
     for i in range(num_feat):
